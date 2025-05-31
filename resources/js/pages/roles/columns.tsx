a0061abe2918f9b9515a03534a691b7740ab5adf
chrystalio <chrystalio.k@outlook.com>
2025-06-01 03:07:52 +0700

    Add role columns

---
+++ resources/js/pages/roles/columns.tsx
@@ -0,0 +1,48 @@
+import type { Role } from '@/types'
+import { ColumnDef } from '@tanstack/react-table'
+import { MoreHorizontal } from 'lucide-react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+
+export const columns: ColumnDef<Role>[] = [
+    {
+        accessorKey: 'name',
+        header: 'Name',
+        cell: ({ row }) => <div className="capitalize">{row.getValue('name')}</div>,
+    },
+
+    {
+        id: 'actions',
+        header: 'Actions',
+        cell: ({ row }) => {
+            const role = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+                </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => console.log('Edit', role.id)}>
+            Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem
+            onClick={() => console.log('Delete', role.id)}
+            className="text-red-500"
+                >
+                Delete
+                </DropdownMenuItem>
+                </DropdownMenuContent>
+                </DropdownMenu>
+        )
+        },
+    },
+]
