c92d6aa963be7eb71d95b610224b8423422a47b4
chrystalio <chrystalio.k@outlook.com>
2025-06-01 03:06:57 +0700

    add `Role` type

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -38,3 +38,10 @@ export interface User {
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
 }
+
+export type Role = {
+    id: number
+    name: string
+    created_at?: string
+    updated_at?: string
+}
