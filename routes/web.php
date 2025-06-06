edfa1239a38ac0ebc1f1a364431a22b028244e1b
chrystalio <chrystalio.k@outlook.com>
2025-06-06 23:33:26 +0700

    Add route for `RolePermissionController`

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\RolePermissionController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::resource('roles', RoleController::class);
         Route::get('/users', [UserController::class, 'index'])->name('admin.users.index');
         Route::put('/users/{user}/roles', [UserController::class, 'updateRoles'])->name('admin.users.updateRoles');
+
+        Route::get('/roles/{role}/permissions', [RolePermissionController::class, 'index']);
+        Route::post('/roles/{role}/permissions', [RolePermissionController::class, 'update']);
+
     });
 });
 
