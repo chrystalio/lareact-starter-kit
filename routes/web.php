dea653fc435ee0c2c0839fd6099b9c062ad8001c
chrystalio <chrystalio.k@outlook.com>
2025-06-10 14:00:36 +0700

    add permissions and route middleware for RolePermissionController

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::prefix('admin')->group(function () {
         Route::resource('roles', RoleController::class)->middleware('can:role.view');
-        Route::get('/users', [UserController::class, 'index'])->name('admin.users.index');
-        Route::put('/users/{user}/roles', [UserController::class, 'updateRoles'])->name('admin.users.updateRoles');
-    
-        Route::get('/roles/{role}/permissions', [RolePermissionController::class, 'index']);
-        Route::post('/roles/{role}/permissions', [RolePermissionController::class, 'update']);
+        Route::get('/users', [UserController::class, 'index'])->name('admin.users.index')->middleware('can:user.view');
+        Route::put('/users/{user}/roles', [UserController::class, 'updateRoles'])->name('admin.users.updateRoles')->middleware('can:user.update');
+
+        Route::get('/roles/{role}/permissions', [RolePermissionController::class, 'index'])->middleware('can:rolepermission.view')->name('admin.roles.permissions.index');
+        Route::post('/roles/{role}/permissions', [RolePermissionController::class, 'update'])->middleware('can:rolepermission.update')->name('admin.roles.permissions.update');
     });
 });
 
