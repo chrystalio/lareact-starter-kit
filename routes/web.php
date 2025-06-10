c98710d855413cb61b798314de16c880dfb2ee42
chrystalio <chrystalio.k@outlook.com>
2025-06-10 16:56:01 +0700

    Add route for updateRoles edit method

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::resource('roles', RoleController::class)->middleware('can:role.view');
         Route::get('/users', [UserController::class, 'index'])->name('admin.users.index')->middleware('can:user.view');
         Route::put('/users/{user}/roles', [UserController::class, 'updateRoles'])->name('admin.users.updateRoles')->middleware('can:user.update');
+        Route::put('/users/{user}/roles', [UserController::class, 'updateRoles'])->name('admin.users.updateRoles')->middleware('can:user.edit');
 
         Route::get('/roles/{role}/permissions', [RolePermissionController::class, 'index'])->middleware('can:rolepermission.view')->name('admin.roles.permissions.index');
         Route::post('/roles/{role}/permissions', [RolePermissionController::class, 'update'])->middleware('can:rolepermission.update')->name('admin.roles.permissions.update');
